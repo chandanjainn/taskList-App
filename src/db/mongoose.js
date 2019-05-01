67f2d1f06fa8b6d5b9e281cd44323c5770ee7fa6
Chandan Jain <c.jainn@ymail.com>
2019-05-02 02:49:25 +0530

    fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const mongoURL = encodeURI(process.env.MONGODB_URL);
 
 mongoose.connect(mongoURL, {
-	uri_decode_auth: true,
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
