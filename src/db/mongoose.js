51023dfd5542aee237ef018fca920a72ca754183
Chandan Jain <c.jainn@ymail.com>
2019-05-02 02:35:08 +0530

    uri decode

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URL, {
+	uri_decode_auth: true,
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
